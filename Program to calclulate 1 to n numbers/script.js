0af65b45c39fdc73f9d8d481867de8e72410bede
Mayank Sharma <mayanksharma800680@gmail.com>
2025-10-30 20:21:00 -0700

    Add files via upload

---
+++ Program to calclulate 1 to n numbers/script.js
@@ -0,0 +1,17 @@
+alert("this website is made by Mayank Sharma");
+console.log("this website is made by Mayank Sharma");
+
+
+
+let quetion = prompt("enter the number to calculate 1 to n");
+let sum = 0;
+for(i=1; i<=quetion; i++){
+    sum = sum + i;
+}
+
+console.log("sum of 1 to" , quetion ,"=", sum);
+alert("sum of 1 to" , quetion ,"=", sum);
+// alert("please open console");
+// alert("to open console right click on mouse select inspect and find console");
+
+
